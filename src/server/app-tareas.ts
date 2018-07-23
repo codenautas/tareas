d9b6060e2c2a17ce0aafe284a216e457d8c6effa
Manuel De la Penna <manueldelapenna@gmail.com>
2018-07-23 15:11:28 -0300

    genero tablas de locks y tokens y paso defConfig a staticConfig

--- src/server/app-tareas.ts
+++ src/server/app-tareas.ts
@@ -18,6 +18,75 @@ export function emergeAppTareas<T extends Constructor<AppBackend>>(Base:T){
         constructor(...args:any[]){ 
             super(...args);
         }
+        configStaticConfig(){
+            super.configStaticConfig();
+            this.setStaticConfig(`
+              server:
+                port: 3032
+                base-url: /tareas
+                session-store: memory
+              db:
+                motor: postgresql
+                host: localhost
+                database: tareas_db
+                schema: tar
+                user: tareas_user
+                search_path: 
+                - tar
+              install:
+                dump:
+                  db:
+                    owner: tareas_owner
+                  admin-can-create-tables: true
+                  enances: inline
+                  skip-content: true
+                  scripts:
+                    post-adapt: 
+                    - event_control_creates.sql
+                    - para-install.sql
+                    - ../node_modules/pg-triggers/lib/recreate-his.sql
+                    - ../node_modules/pg-triggers/lib/table-changes.sql
+                    - ../node_modules/pg-triggers/lib/function-changes-trg.sql
+                    - ../node_modules/pg-triggers/lib/enance.sql
+              login:
+                table: usuarios
+                userFieldName: usuario
+                passFieldName: md5clave
+                rolFieldName: rol
+                infoFieldList: [usuario, rol]
+                activeClausule: activo
+                plus:
+                  maxAge-5-sec: 5000    
+                  maxAge: 864000000
+                  maxAge-10-day: 864000000
+                  allowHttpLogin: true
+                  fileStore: false
+                  loginForm:
+                    formTitle: Tareas
+                    usernameLabel: usuariox
+                    passwordLabel: md5clave
+                    buttonLabel: entrar
+                    formImg: img/login-lock-icon.png
+                    autoLogin: true
+                  chPassForm:
+                    usernameLabel: usuario
+                    oldPasswordLabel: clave anterior
+                    newPasswordLabel: nueva clave
+                    repPasswordLabel: repetir nueva clave
+                    buttonLabel: Cambiar
+                    formTitle: Cambio de clave
+                messages:
+                  userOrPassFail: el nombre de usuario no existe o la clave no corresponde
+                  lockedFail: el usuario se encuentra bloqueado
+                  inactiveFail: es usuario está marcado como inactivo
+              client-setup:
+                cursors: true
+                lang: es
+                menu: true
+                grid-buffer: idbx
+                version: 0.2
+            `);
+        }
         getProcedures(){
             var be = this;
             return super.getProcedures().then(function(procedures){
@@ -49,13 +118,13 @@ export function emergeAppTareas<T extends Constructor<AppBackend>>(Base:T){
             return menu;
         }
         getTables(){
-            return [
+            return super.getTables().concat([
                 'usuarios',
                 'estados',
                 'objetivos',
                 'tareas',
                 'detalles',
-            ]
+            ]);
         }
     }
 }
